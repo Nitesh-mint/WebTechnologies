0842d1846370f7686e313dc85cc8d6620ad7e342
Nitesh-mint <nitesh.raya058@gmail.com>
2023-01-15 08:59:10 +0545

    initial commit

---
+++ web_lab/Variables_DataTypes.php
@@ -0,0 +1,14 @@
+<?php
+    $name = "Nitesh"; //string 
+    $age = 20; //integer
+    $height = 5.5; //float
+    $awesome = true; //boolean
+
+    //composite
+    $arr = array(0,1,2,3,4,5);
+    $obj = new className; //object 
+
+    //special
+    $zero = NULL; //Null
+    $connect = mysqli_connect("localhost","root","password","db_name");
+?>
---
+++ web_lab/mail.php
@@ -0,0 +1,10 @@
+<?php
+    $to = "receiver@gmail.com";
+    $subject = "subject of the mail";
+    $meessage = "detailed message and information of the mail";//each line shouldnot exceed 70 chars
+    $header = "sender@gmail.com";
+    $parameter = "sendmail_path"; // for specifying the additional parameters defined in the sendmail_path configuration setting.
+
+    //using mail function
+    mail($to,$subject,$message,[$headers],[$parameter]);
+?>
